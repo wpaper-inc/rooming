8c58d3c6cc3aa3fe5f79b9410305135f3a538fdb
Kousuke Takeuchi <k.takeuchi@warrantee.co.jp>
2018-11-12 23:38:11 +0900

    add newrelic config

--- rooming/rooming/wsgi.py
+++ rooming/rooming/wsgi.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+import newrelic.agent
+newrelic.agent.initialize('newrelic.ini')
 
+from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rooming.settings')
 
 application = get_wsgi_application()
+application = newrelic.agent.wsgi_application()(application)
